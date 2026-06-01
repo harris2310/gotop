a3c67be950942757488c8b0a8ae7da9d758e17c0
harris2310 <harris2310@hotmail.com>
2026-06-01 21:58:29 +0300

    feat: read linux cpu temp and print in a loop

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"time"
 )
+
+func readTemp(core int, buff []byte) (temp int) {
+	file, err := os.OpenFile("/sys/class/thermal/thermal_zone0/hwmon"+strconv.Itoa(core)+"/temp1_input", os.O_RDONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	n, err := file.Read(buff)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return n
+}
 
 func main() {
 	fmt.Println("hello sir")
@@ -11,6 +27,15 @@ func main() {
 	for sum < 10 {
 		fmt.Println("------")
 		sum += 1
+	}
+	for {
+
+		buff := make([]byte, 128)
+
+		var len int = readTemp(1, buff)
+		fmt.Println(string(buff[:len-4]) + "C")
+		time.Sleep(1 * time.Second)
+
 	}
 	os.Exit(1)
 }
